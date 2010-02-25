b080a1e4f5fbb62f3a7629e31749664338a64a13
Erik Garrison <erik@humu.bc.edu>
2010-02-25 12:08:27 -0500

    minor fix to TRY CATCH macros
    Added __FILE__ name to CATCH error report.

--- src/TryCatch.h
+++ src/TryCatch.h
@@ -9,7 +9,8 @@
 #define CATCH \
     catch (std::out_of_range outOfRange) { \
         cerr << "exception: " << outOfRange.what() \
-        << " at line " << __LINE__ << endl; \
+        << " at line " << __LINE__ \
+        << " in file " << __FILE__ << endl; \
         abort(); \
     }
 #endif
