c6643999b2270c429af4a6f537c38fc149a56e4a
Erik Garrison <erik.garrison@bc.edu>
2010-09-23 11:32:44 -0400

    compile fix, allows large file support on Mac
    off_t is a 64-bit type on the mac.  Thus it does not have __int64 or
    __off64_t.  This should allow compilation on Mac OS X.  However, users
    will have to disable the -staic flag in Makefile as Mac OS X's toolchain
    does not support static compilation.

--- src/LargeFileSupport.h
+++ src/LargeFileSupport.h
@@ -4,6 +4,10 @@
 #define ftell64(a)     _ftelli64(a)
 #define fseek64(a,b,c) _fseeki64(a,b,c)
 typedef __int64 off_type;
+#elif defined(__APPLE__)
+#define ftell64(a)     ftello(a)
+#define fseek64(a,b,c) fseeko(a,b,c)
+typedef off_t off_type;
 #else
 #define ftell64(a)     ftello(a)
 #define fseek64(a,b,c) fseeko(a,b,c)
