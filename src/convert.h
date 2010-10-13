8b90a12099b4ce20349487eda3362eef672291f7
Erik Garrison <erik.garrison@bc.edu>
2010-10-13 13:57:59 -0400

    added convert.h templated string conversion

---
+++ src/convert.h
@@ -0,0 +1,10 @@
+#include <sstream>
+
+// converts the string into the specified type, setting r to the converted
+// value and returning true/false on success or failure
+template<typename T>
+bool convert(const string& s, T& r) {
+    istringstream iss(s);
+    iss >> r;
+    return (iss.fail() || iss.tellg() != s.size()) ? false : true;
+}
