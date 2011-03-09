3e416cccd5bc149b0adbe92c55cebabe682b4fd4
Erik Garrison <erik.garrison@bc.edu>
2011-03-09 14:33:45 -0500

    region spec generation from fasta index file
    This script can be used to make region specifiers which can in turn be
    used in a map/reduce operation to map freebayes across a whole genome.
    One would launch a single job per region specifier, changing no other
    parameters, and output to a different file, then merge the files.

---
+++ scripts/fasta_generate_regions.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+import sys
+
+
+if len(sys.argv) == 1:
+    print "usage: ", sys.argv[0], " <fasta index file> <region size>"
+    print "generates a list of freebayes/bamtools region specifiers on stdout"
+    print "intended for use in creating cluster jobs"
+    exit(1)
+
+fasta_index_file = open(sys.argv[1])
+region_size = int(sys.argv[2])
+
+for line in fasta_index_file:
+
+    fields = line.strip().split("\t")
+
+    chrom_name = fields[0]
+    chrom_length = int(fields[1])
+    region_start = 0
+
+    while region_start < chrom_length:
+        start = region_start
+        end = region_start + region_size
+        if end > chrom_length:
+            end = chrom_length
+        print chrom_name + ":" + str(region_start) + ".." + str(end)
+        region_start = end
+
+
