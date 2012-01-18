3cb41894c3850a863a8d66cca51d3c3da4d4961e
Erik Garrison <erik.garrison@bc.edu>
2012-01-18 18:57:52 -0500

    update of bamtools

--- src/AlleleParser.cpp
+++ src/AlleleParser.cpp
@@ -38,11 +38,6 @@ void AlleleParser::openBams(void) {
         }
     }
     
-    // set no index caching if we are only making one jump
-    if (targets.size() == 1) {
-        bamMultiReader.SetIndexCacheMode(BamIndex::NoIndexCaching);
-    }
-
     if (parameters.useStdin) {
         if (!bamMultiReader.Open(parameters.bams)) {
             ERROR("Could not read BAM data from stdin");
