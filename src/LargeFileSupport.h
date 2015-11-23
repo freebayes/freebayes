b32a3a1be2f3077bab1f544cfbf53a26eefc7cbd
Jason Bacon <jwbacon@tds.net>
2015-11-23 12:36:30 -0600

    Separate Makefiles for building with external libraries

--- src/LargeFileSupport.h
+++ src/LargeFileSupport.h
@@ -4,7 +4,7 @@
 #define ftell64(a)     _ftelli64(a)
 #define fseek64(a,b,c) _fseeki64(a,b,c)
 typedef __int64 off_type;
-#elif defined(__APPLE__)
+#elif defined(__APPLE__) || defined(__FreeBSD__)
 #define ftell64(a)     ftello(a)
 #define fseek64(a,b,c) fseeko(a,b,c)
 typedef off_t off_type;
