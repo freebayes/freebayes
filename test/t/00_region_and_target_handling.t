freebayes/freebayes

commit 840b4124fed332fc62848bc409c6f7193022d279

resolves #188 by simplifying alleleparser position stepping
This disables a (buggy) optimization where the parser would jump to the next
position with alignments or input alleles. This should have minimal effect on
performance at a benefit of greatly decreasing the complexity of the position
increment process.

src/AlleleParser.cpp
@@ -2517,6 +2517,8 @@ void AlleleParser::removePreviousAlleles(vector<Allele*>& alleles) {
 // false otherwise
 bool AlleleParser::toNextTarget(void) {
 
+    DEBUG("to next target");
+
     clearRegisteredAlignments();
 
     // reset haplotype length; there is no last call in this sequence; it isn't relevant
@@ -2711,88 +2713,71 @@ void AlleleParser::clearRegisteredAlignments(void) {
 // if none exist, return false
 //
 bool AlleleParser::toNextPosition(void) {
-
-    // either bail out
+    // is this our first position? (indicated by empty currentSequenceName)
+    // if so, load it up
+    bool first_pos = false;
     if (currentSequenceName.empty()) {
         DEBUG("loading first target");
         if (!toNextTarget()) {
             return false;
         }
-    } 
-    // or step to the next position
-    else {
-        // if there is no data in the pile
-        // and the curentalignment is far away
-        // and there are no more variants right here
-        // jump the parser to the next position with an input allele or data
-        if (registeredAlignments.empty() && !hasInputVariantAllelesAtCurrentPosition()) {
-            if (currentAlignment.GetEndPosition() < currentPosition) {
-                hasMoreAlignments = getFirstAlignment();
-                if (!hasMoreAlignments && !hasMoreInputVariants()) return false;
-            }
-            // if this is as far as we can go, bail
-            long int lastPosition = currentPosition;
-            loadNextPositionWithAlignmentOrInputVariant(currentAlignment);
-            if (lastPosition == currentPosition) return false;
-            justSwitchedTargets = true;
-        } else {
-            ++currentPosition;
-        }
+        first_pos = true;
     }
 
-    // if we've run off the right edge of a target
-    if (!targets.empty() && currentPosition > currentTarget->right) { // time to move to a new target
-        DEBUG("next position " << (long int) currentPosition <<  " outside of current target right bound " << currentTarget->right + 1);
-        // try to get to the next one, and if this fails, bail out
-        if (!toNextTarget()) {
-            DEBUG("no more targets, finishing");
-            return false;
-        }
-    }
-
-    // in the stdin, or no targets case
     // here we assume we are processing an entire BAM or one contiguous region
-    if ((parameters.useStdin && targets.empty()) || targets.empty()) {
+    if (parameters.useStdin || targets.empty()) {
         // here we loop over unaligned reads at the beginning of a target
         // we need to get to a mapped read to figure out where we are
         while (hasMoreAlignments && !currentAlignment.IsMapped()) {
             hasMoreAlignments = bamMultiReader.GetNextAlignment(currentAlignment);
         }
-        // now, if the current position of this alignment is outside of the reference sequence length, switch references
-        if (hasMoreAlignments) {
+        // determine if we have more alignments or not
+        if (!hasMoreAlignments) {
+            if (hasMoreInputVariants() || hasInputVariantAllelesAtCurrentPosition()) {
+                // continue as we have more variants
+                DEBUG("continuing because we have more input variants");
+                loadNextPositionWithInputVariant();
+            } else if (registeredAlignments.empty()) {
+                DEBUG("no more alignments in input");
+                return false;
+            } else if (currentPosition >= currentSequence.size() + currentSequenceStart) {
+                DEBUG("no more alignments in input");
+                DEBUG("at end of sequence");
+                return false;
+            } else {
+                ++currentPosition;
+            }
+        } else {
+            // step the position
+            ++currentPosition;
+            // if the current position of this alignment is outside of the reference sequence length
+            // we need to switch references
             if (currentPosition >= reference.sequenceLength(currentSequenceName)
                 || registeredAlignments.empty() && currentRefID != currentAlignment.RefID) {
                 DEBUG("at end of sequence");
                 clearRegisteredAlignments();
-                loadReferenceSequence(currentAlignment);
+                loadNextPositionWithAlignmentOrInputVariant(currentAlignment);
                 justSwitchedTargets = true;
             }
-        // if we have run out of alignments
-        } else if (!hasMoreAlignments) {
-            if (registeredAlignments.empty()) {
-                if (hasMoreInputVariants() || hasInputVariantAllelesAtCurrentPosition()) {
-                    //loadNextPositionWithAlignmentOrInputVariant(currentAlignment);
-                    //justSwitchedTargets = true;
-                } else {
-                    DEBUG("no more alignments in input");
-                    return false;
-                }
-            } else if (currentPosition >= currentSequence.size() + currentSequenceStart) {
-                if (hasMoreInputVariants() || hasInputVariantAllelesAtCurrentPosition()) {
-                    //loadNextPositionWithAlignmentOrInputVariant(currentAlignment);
-                    //justSwitchedTargets = true;
-                } else {
-                    DEBUG("no more alignments in input");
-                    DEBUG("at end of sequence");
-                    return false;
-                }
+        }
+    } else {
+        // or if it's not we should step to the next position
+        ++currentPosition;
+        // if we've run off the right edge of a target, jump
+        if (currentPosition > currentTarget->right) {
+            // time to move to a new target
+            DEBUG("next position " << (long int) currentPosition
+                  <<  " outside of current target right bound " << currentTarget->right + 1);
+            // try to get to the next one, and if this fails, bail out
+            if (!toNextTarget()) {
+                DEBUG("no more targets, finishing");
+                return false;
             }
+            justSwitchedTargets = true;
         }
     }
 
     // so we have to make sure it's still there (this matters in low-coverage)
-    DEBUG2("updating reference sequence cache");
-    //preserveReferenceSequenceWindow(CACHED_REFERENCE_WINDOW);
     currentReferenceBase = currentReferenceBaseChar();
 
     // handle the case in which we don't have targets but in which we've switched reference sequence
@@ -3517,6 +3502,7 @@ bool AlleleParser::getNextAlleles(Samples& samples, int allowedAlleleTypes) {
         if (!toNextPosition()) {
             return false;
         } else {
+            // triggers cleanup
             if (justSwitchedTargets) {
                 nextPosition = 0;
                 justSwitchedTargets = false;

paper/main.blg
@@ -1,4 +1,4 @@
-This is BibTeX, Version 0.99d (TeX Live 2013/Debian)
+This is BibTeX, Version 0.99d (TeX Live 2015/dev/Debian)
 Capacity: max_strings=35307, hash_size=35307, hash_prime=30011
 The top-level auxiliary file: main.aux
 The style file: genome_research.bst

src/freebayes.cpp
@@ -152,11 +152,13 @@ int main (int argc, char *argv[]) {
             DEBUG2("after trace generation");
         }
 
+        /*
         if (!parser->inTarget()) {
             DEBUG("position: " << parser->currentSequenceName << ":" << (long unsigned int) parser->currentPosition + 1
                   << " is not inside any targets, skipping");
             continue;
         }
+        */
 
         int coverage = countAlleles(samples);
 

commit 8d0068e7ad3ded77fe795156c8ad9defc27efced

Fix region tests and ensure that they are used
* Region tests were not actually being run by 'make test'
* Region tests were failing
* Fix variant detection at start of region (caused by commit 840b412)
* Add tests for IGV-style regions (commit a412676)
* Refactor tests are little
* Add -S for old samtools

This is an *attempted* fix for the first-of-target problem; I cannot
be sure that it is a correct fix. There is another line that
increments the counter just above, but from what I can tell this one
should stay as it is.

src/AlleleParser.cpp
@@ -674,7 +674,7 @@ void AlleleParser::loadTargets(void) {
             size_t foundRangeSep = region.find(sep, foundFirstColon);
             if (foundRangeSep == string::npos) {
                 sep = "-";
-                foundRangeSep = region.find("-", foundFirstColon);
+                foundRangeSep = region.find(sep, foundFirstColon);
             }
             if (foundRangeSep == string::npos) {
                 startPos = stringToInt(region.substr(foundFirstColon + 1));
@@ -2765,7 +2765,9 @@ bool AlleleParser::toNextPosition(void) {
             }
         } else {
             // step the position
-            ++currentPosition;
+            if (!first_pos) {
+                ++currentPosition;
+            }
             // if the current position of this alignment is outside of the reference sequence length
             // we need to switch references
             if (currentPosition >= reference.sequenceLength(currentSequenceName)
@@ -2778,7 +2780,9 @@ bool AlleleParser::toNextPosition(void) {
         }
     } else {
         // or if it's not we should step to the next position
-        ++currentPosition;
+        if (!first_pos) {
+            ++currentPosition;
+        }
         // if we've run off the right edge of a target, jump
         if (currentPosition > currentTarget->right) {
             // time to move to a new target

test/region-and-target-handling.t
@@ -1,104 +1,0 @@
-#!/bin/bash
-# vi: set ft=sh :
-test=$(dirname $0)
-root=$(dirname $0)/..
-source $test/test-simple-bash/lib/test-simple.bash \
-    tests 8
-
-PATH=$root/bin:$PATH
-
-ref=$test/`basename $0`.ref
-alt=$test/`basename $0`.alt
-bam=$test/`basename $0`.bam
-bed=$test/`basename $0`.bed
-vcf=$test/`basename $0`.vcf
-
-trap 'rm -f $ref* $alt $bam* $bed $vcf' EXIT
-
-# 01234567	start
-# ATCGGCTA
-# 12345678	end
-
-cat >$ref <<REF
->ref
-ATCGGCTA
-REF
-samtools faidx $ref
-
-cat >$alt <<ALT
->alt
-GTTAGGTT
-ALT
-
-samtools view -b - >$bam <<SAM
-@HD	VN:1.5	SO:coordinate
-@SQ	SN:ref	LN:8
-alt	0	ref	1	30	1X1=2X1=1X1=1X	*	0	0	GTTAGGTT	*
-SAM
-samtools index $bam
-
-cat >$bed <<BED
-ref	0	1	first_base
-ref	2	4	third_and_fourth_base
-ref	5	6	sixth_base
-ref	7	8	eigth_base
-BED
-
-cat >$vcf <<VCF
-##fileformat=VCFv4.1
-##INFO=<ID=NAME,Number=0,Type=String,Description="Test name">
-#CHROM	POS	ID	REF	ALT	QUAL	FILTER	INFO
-ref	1	.	A	G	1234	PASS	NAME=first base
-ref	2	.	T	.	1234	PASS	NAME=second base
-ref	3	.	C	T	1234	PASS	NAME=third base
-ref	4	.	G	A	1234	PASS	NAME=fourth base
-ref	5	.	G	.	1234	PASS	NAME=fifth base
-ref	6	.	C	G	1234	PASS	NAME=sixth base
-ref	7	.	T	.	1234	PASS	NAME=seventh base
-ref	8	.	A	T	1234	PASS	NAME=eigth base
-VCF
-
-PS4='\n+ '
-
-function run_freebayes() {
-    ($root/bin/freebayes "$@" \
-        --haplotype-length 0 --min-alternate-count 1 \
-        --min-alternate-fraction 0 --pooled-continuous --report-monomorphic \
-        --ploidy 1 \
-        -f $ref $bam \
-        | grep -vE '^#' | cut -f1-5)
-}
-
-if [[ -n $TEST_DEBUG ]]; then
-    cat $ref >&2
-    cat $bed >&2
-    cat $vcf >&2
-    vcfannotate --bed $bed --key MATCH $vcf >&2
-    vcfintersect --bed $bed $vcf >&2
-    bedtools intersect -a $vcf -b $bed >&2
-fi
-
-[[ -z $(run_freebayes --region ref:4-5 --region ref:6-7) ]]; ok $? 'ref:4-5 ref:6-7 are empty'
-[[ -z $(run_freebayes --region ref:4   --region ref:6)   ]]; ok $? 'ref:4   ref:6   are empty'
-
-expected=`cat <<END
-ref	6	.	C	G
-ref	8	.	A	T
-END
-`
-[[ $(run_freebayes --region ref:5-6 --region ref:7-8) == $expected ]]; ok $? 'ref:5-6 ref:7-8'
-[[ $(run_freebayes --region ref:5   --region ref:7)   == $expected ]]; ok $? 'ref:5   ref:7'
-[[ $(run_freebayes --region ref:5-)                   == $expected ]]; ok $? 'ref:5-'
-
-expected=`cat <<END
-ref	1	.	A	G
-ref	3	.	CG	TA
-ref	6	.	C	G
-ref	8	.	A	T
-END
-`
-[[ $(run_freebayes --targets $bed) == $expected ]]; ok $? "--targets $bed"
-[[ $(run_freebayes --region ref)   == $expected ]]; ok $? "--region ref"
-
-[[ $(run_freebayes --region ref:1-20 2>&1) =~ "Target region coordinates (ref 1 19) outside of reference sequence bounds (ref 8)" ]]
-    ok $? 'region outside of bounds error'

test/t/00_region_and_target_handling.t
@@ -1,0 +1,145 @@
+#!/usr/bin/env bash
+# vi: set ft=sh :
+
+test=$(dirname $0)/..
+root=$(dirname $0)/../..
+
+source $test/test-simple-bash/lib/test-simple.bash \
+    tests 11
+
+PATH=$root/bin:$PATH
+
+ref=$test/$(basename $0).ref
+alt=$test/$(basename $0).alt
+bam=$test/$(basename $0).bam
+bed=$test/$(basename $0).bed
+vcf=$test/$(basename $0).vcf
+
+trap 'rm -f $ref* $alt $bam* $bed $vcf' EXIT
+
+# 01234567890	start
+# ATCGGCTAAAA	ref
+# GTTAGGTTAAC	alt
+# 12345678901	end
+
+cat >$ref <<REF
+>ref
+ATCGGCTAAAA
+REF
+samtools faidx $ref
+
+cat >$alt <<ALT
+>alt
+GTTAGGTTAAC
+ALT
+
+samtools view -S -b - >$bam <<SAM
+@HD	VN:1.5	SO:coordinate
+@SQ	SN:ref	LN:11
+alt	0	ref	1	30	1X1=2X1=1X1=1X2=1X	*	0	0	GTTAGGTTAAC	*
+SAM
+samtools index $bam
+
+cat >$bed <<BED
+ref	0	1	first_base
+ref	2	4	third_and_fourth_base
+ref	5	6	sixth_base
+ref	7	8	eighth_base
+ref	10	11	eleventh base
+BED
+
+cat >$vcf <<VCF
+##fileformat=VCFv4.1
+##INFO=<ID=NAME,Number=0,Type=String,Description="Test name">
+#CHROM	POS	ID	REF	ALT	QUAL	FILTER	INFO
+ref	1	.	A	G	1234	PASS	NAME=first base
+ref	2	.	T	.	1234	PASS	NAME=second base
+ref	3	.	C	T	1234	PASS	NAME=third base
+ref	4	.	G	A	1234	PASS	NAME=fourth base
+ref	5	.	G	.	1234	PASS	NAME=fifth base
+ref	6	.	C	G	1234	PASS	NAME=sixth base
+ref	7	.	T	.	1234	PASS	NAME=seventh base
+ref	8	.	A	T	1234	PASS	NAME=eighth base
+ref	9	.	A	.	1234	PASS	NAME=ninth base
+ref	10	.	A	.	1234	PASS	NAME=tenth base
+ref	11	.	A	C	1234	PASS	NAME=eleventh base
+VCF
+
+PS4='\n+ '
+
+function run_freebayes() {
+    ($root/bin/freebayes "$@" \
+        --haplotype-length 0 --min-alternate-count 1 \
+        --min-alternate-fraction 0 --pooled-continuous --report-monomorphic \
+        --ploidy 1 \
+        -f $ref $bam \
+        | grep -vE "^#" | cut -f1-5)
+}
+
+if [[ -n $TEST_DEBUG ]]; then
+    cat $ref >&2
+    cat $bed >&2
+    cat $vcf >&2
+    vcfannotate --bed $bed --key MATCH $vcf >&2
+    vcfintersect --bed $bed $vcf >&2
+    bedtools intersect -a $vcf -b $bed >&2
+fi
+
+
+output=$(run_freebayes --region ref:4-5 --region ref:6-7)
+ok [ -z "$output" ] "ref:4-5 ref:6-7 are empty" || echo "$output"
+
+output=$(run_freebayes --region ref:4 --region ref:6)
+ok [ -z "$output" ] "ref:4 ref:6 are empty" || echo "$output"
+
+expected=$(cat <<END
+ref	6	.	C	G
+ref	8	.	A	T
+END
+)
+output=$(run_freebayes --region ref:5-6 --region ref:7-8)
+ok [ "$output" == "$expected" ] "ref:5-6 ref:7-8" || echo "$output"
+
+output=$(run_freebayes --region ref:5 --region ref:7)
+ok [ "$output" == "$expected" ] "ref:5 ref:7" || echo "$output"
+
+expected=$(cat <<END
+ref	6	.	C	G
+ref	8	.	A	T
+ref	11	.	A	C
+END
+)
+output=$(run_freebayes --region ref:5-)
+ok [ "$output" == "$expected" ] "ref:5-" || echo "$output"
+
+expected=$(cat <<END
+ref	1	.	A	G
+ref	3	.	CG	TA
+ref	6	.	C	G
+ref	8	.	A	T
+ref	11	.	A	C
+END
+)
+output=$(run_freebayes)
+ok [ "$output" == "$expected" ] "full output" || echo "$output"
+
+output=$(run_freebayes --targets $bed)
+ok [ "$output" == "$expected" ] "--targets $bed" || echo "$output"
+
+output=$(run_freebayes --region ref)
+ok [ "$output" == "$expected" ] "--region ref" || echo "$output"
+
+# commas as separators: should really be 1,000
+expected=$(cat <<END
+ref	11	.	A	C
+END
+)
+output=$(run_freebayes --region ref:1,0)
+ok [ "$output" == "$expected" ] "ref:1,0-" || echo "$output"
+
+output=$(run_freebayes --region ref:1,0-1,1)
+ok [ "$output" == "$expected" ] "ref:1,0-1,1" || echo "$output"
+
+expected="ERROR(freebayes): Target region coordinates (ref 1 20) outside of reference sequence bounds (ref 11) terminating."
+output=$(run_freebayes --region ref:1-20 2>&1)
+ok [ "$output" == "$expected" ] "region outside of bounds error" || echo "$output"

test/t/01_call_variants.t
@@ -62,7 +62,7 @@ q	12119	12130
 EOF
 
 is $(freebayes -f tiny/q.fa tiny/NA12878.chr22.tiny.bam -t targets.bed | grep -v "^#" | wc -l) $by_region "a targets bed file can be used with the same effect as running by region"
-#rm targets.bed
+rm targets.bed
 
 
 is $(samtools view -u tiny/NA12878.chr22.tiny.bam | freebayes -f tiny/q.fa --stdin | grep -v "^#" | wc -l) \
@@ -96,7 +96,7 @@ gzip -c tiny/q.fa >tiny/q.fa.gz
 cp tiny/q.fa.fai tiny/q.fa.gz.fai
 freebayes -f tiny/q.fa.gz -@ tiny/q_spiked.vcf.gz -r q:1-10000 -l - < tiny/NA12878.chr22.tiny.bam >/dev/null 2>/dev/null
 is $? 1 "freebayes bails out when given a gzipped or corrupted reference"
-rm tiny/q.fa.gz.*
+rm tiny/q.fa.gz*
 
 is $(freebayes -f tiny/q.fa tiny/NA12878.chr22.tiny.bam | grep -v "^#" | wc -l) $(freebayes-parallel tiny/q.regions 2 -f tiny/q.fa tiny/NA12878.chr22.tiny.bam | grep -v "^#" | wc -l) "running in parallel makes no difference"
 