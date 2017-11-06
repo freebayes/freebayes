ba4b26cf934b76edd92509be0f15996354d4f3bd
rainbowgoblin <jessica@biomatters.com>
2017-11-06 14:35:03 +1300

    Support for compilation with Cygwin 64

--- src/LargeFileSupport.h
+++ src/LargeFileSupport.h
@@ -4,6 +4,10 @@
 #define ftell64(a)     _ftelli64(a)
 #define fseek64(a,b,c) _fseeki64(a,b,c)
 typedef __int64 off_type;
+#elif defined(__CYGWIN__)
+#define ftell64(a)     ftell(a)
+#define fseek64(a,b,c) fseek(a,b,c)
+typedef __int64_t off_type;
 #elif defined(__APPLE__)
 #define ftell64(a)     ftello(a)
 #define fseek64(a,b,c) fseeko(a,b,c)
