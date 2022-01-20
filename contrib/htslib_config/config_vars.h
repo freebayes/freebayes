b5a69a708aa230c2b24285d641967a94b126c30c
Pjotr Prins <pjotr.public01@thebird.nl>
2022-01-20 09:28:01 -0600

    build against a recent htslib

---
+++ contrib/htslib_config/config_vars.h
@@ -0,0 +1,5 @@
+#define HTS_CC "gcc"
+#define HTS_CPPFLAGS ""
+#define HTS_CFLAGS "-g -Wall -O2 -fvisibility=hidden"
+#define HTS_LDFLAGS "-fvisibility=hidden"
+#define HTS_LIBS "-lz -lm -lbz2 -llzma -lcurl"
