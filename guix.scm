037117da997386655bed28dac97ca677c937a357
Pjotr Prins <pjotr.public01@thebird.nl>
2025-05-17 20:04:19 +0200

    guix: now building with a late checkout of vcflib

--- guix.scm
+++ guix.scm
@@ -15,36 +15,97 @@
 ;;   ninja
 ;;   ninja test
 
-(use-modules
-  ((guix licenses) #:prefix license:)
-  (guix gexp)
-  (guix packages)
-  (guix git-download)
-  (guix build-system meson)
-  (gnu packages algebra)
-  (gnu packages assembly)
-  (gnu packages base)
-  (gnu packages compression)
-  (gnu packages bioinformatics)
-  (gnu packages build-tools)
-  (gnu packages curl)
-  (gnu packages gcc)
-  (gnu packages gdb)
-  (gnu packages llvm)
-  (gnu packages ninja)
-  (gnu packages parallel)
-  (gnu packages perl)
-  (gnu packages perl6)
-  (gnu packages pkg-config)
-  (gnu packages python)
-  (srfi srfi-1)
-  (ice-9 popen)
-  (ice-9 rdelim))
+(define-module (guix)
+  #:use-module (ice-9 popen)
+  #:use-module (ice-9 rdelim)
+  #:use-module (srfi srfi-1)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (guix build-system cmake) ; for vcflib
+  #:use-module (guix build-system meson)
+  #:use-module (guix download)
+  #:use-module (guix gexp)
+  #:use-module (guix git-download)
+  #:use-module (guix packages)
+  #:use-module (gnu packages algebra)
+  #:use-module (gnu packages assembly)
+  #:use-module (gnu packages base)
+  #:use-module (gnu packages bioinformatics)
+  #:use-module (gnu packages build-tools)
+  #:use-module (gnu packages check)
+  #:use-module (gnu packages compression)
+  #:use-module (gnu packages curl)
+  #:use-module (gnu packages gcc)
+  #:use-module (gnu packages gdb)
+  #:use-module (gnu packages haskell-xyz) ; pandoc for help files
+  #:use-module (gnu packages llvm)
+  #:use-module (gnu packages ninja)
+  #:use-module (gnu packages parallel)
+  #:use-module (gnu packages perl)
+  #:use-module (gnu packages perl6)
+  #:use-module (gnu packages pkg-config)
+  #:use-module (gnu packages python)
+  #:use-module (gnu packages python-xyz) ; for pybind11
+  #:use-module (gnu packages ruby)
+  #:use-module (gnu packages time)
+  #:use-module (gnu packages tls)
+  #:use-module (gnu packages zig))
 
 (define %source-dir (dirname (current-filename)))
 
 (define %git-commit
     (read-string (open-pipe "git show HEAD | head -1 | cut -d ' ' -f 2" OPEN_READ)))
+
+(define-public vcflib-github ;; should update upstream
+  (let ((commit
+         "288cb6eef7a820115c50e7d45ce25d8dc59fbafd"
+                ))
+    (package
+     (name "vcflib-github")
+     (version (string-append "1.0.13-" (string-take commit 7)))
+     (source
+      (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/vcflib/vcflib/")
+             (commit commit)
+             (recursive? #t)))
+       (file-name (string-append name "-" version "-checkout"))
+       (sha256
+        (base32
+         "1cmxcvw3qkzh5lf2rg212zalsrz7y8gxa3snbi4c22z9j3x9iqah"
+        ))))
+    (build-system cmake-build-system)
+    (arguments
+     `(#:tests? #f
+       #:configure-flags
+       ,#~(list
+           "-DCMAKE_BUILD_TYPE=Debug"
+           "-DZIG=OFF")))
+    (inputs
+     (list
+       curl
+       fastahack  ;; dev version not in Debian
+       htslib ;; disable to test local build - with local package below
+       pandoc ; for man pages
+       perl
+       python
+       python-pytest
+       pybind11
+       ruby ; for man pages
+       smithwaterman
+       tabixpp
+       time ; for tests
+       wfa2-lib ; alternative:  cmake  -DCMAKE_BUILD_TYPE=Debug -DWFA_GITMODULE=ON -DZIG=ON ..
+       xz
+       ;; zig-0.14
+       ))
+    (native-inputs
+     `(("pkg-config" ,pkg-config)))
+    (home-page "https://github.com/vcflib/vcflib/")
+    (synopsis "Library for parsing and manipulating VCF files")
+    (description "Vcflib")
+    (license license:expat))))
+
 
 (define-public freebayes-git
   (package
@@ -66,7 +127,7 @@
        ("simde" ,simde)
        ("smithwaterman" ,smithwaterman) ; vcflib shared lib dependency ; bundle for Debian
        ("tabixpp" ,tabixpp)    ; for htslib
-       ("vcflib" ,vcflib)      ; for testing freebayes-parallel
+       ("vcflib-github" ,vcflib-github)  ; for includes and testing freebayes-parallel
        ("wfa2-lib" ,wfa2-lib)  ; vcflib shared lib dependency
        ("which" ,which)        ; for version
        ("xz-static" ,xz "static")     ; for static builds
